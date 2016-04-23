9664a56368a47e9baa15ed2c952a4ad4a9b8d59e
joe-re <joe.tialtngo@gmail.com>
2016-04-24 05:10:00 +0900

    Creating a Schema( and implement cli)

---
+++ cli.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+require 'graphql'
+require 'migration'
+require 'post'
+require 'seed'
+require 'graphql_schema'
+
+Schema = GraphQL::Schema.new(query: QueryRoot)
+result_hash = Schema.execute(ARGV[0])
+p result_hash
